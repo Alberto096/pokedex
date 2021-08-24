dca2eda981d322f282dcd51688a8460b3b513abe
Alberto Juarez <46580289+Alberto096@users.noreply.github.com>
2021-08-24 13:26:20 -0500

    Pokemon ID

--- src/pages/pokemon/Pokemon.tsx
+++ src/pages/pokemon/Pokemon.tsx
@@ -12,8 +12,9 @@ import axios from 'axios';
 import { ApiUrl } from '../../constants/constants';
 import { Button } from 'baseui/button';
 import { Link } from 'react-router-dom';
-import { Alert, ArrowLeft, ArrowRight } from 'baseui/icon';
-import { ListItem, ListItemLabel } from 'baseui/list';
+import { ArrowLeft, ArrowRight } from 'baseui/icon';
+import { Tag, VARIANT, SIZE } from "baseui/tag";
+import { ButtonGroup, SHAPE } from "baseui/button-group";
 // import { ArrowLeft, ArrowRight } from 'baseui/icon';
 
 type PokemonProps = {
@@ -30,18 +31,19 @@ const PokemonId: React.FC<PokemonProps> = ({ match }: PokemonProps) => {
     const [game_indices, setGame_indices] = useState([]);
     const [sprites, setSprites] = useState([]);
     const [types, setTypes] = useState([]);
+    const [moves, setMoves] = useState([]);
 
-    console.log(apisUrl)
+
 
     const ResultExtrac = (data: any) => {
-        console.log(data);
         setNext(data?.id + 1)
         setPrevious(data?.id + 2)
         setName(data?.name)
-        setAbilities(data)
-        setGame_indices(data)
-        setSprites(data)
-        setTypes(data)
+        setAbilities(data?.abilities)
+        setGame_indices(data?.game_indices)
+        setSprites(data?.sprites?.versions)
+        setTypes(data?.types)
+        setMoves(data?.moves)
     }
 
     useEffect(() => {
@@ -108,7 +110,34 @@ const PokemonId: React.FC<PokemonProps> = ({ match }: PokemonProps) => {
                             <div className={css({ background: 'transparent', padding: '16px' })}></div>
                         </Cell>
                         <Cell span={[12, 12, 8]}>
-
+                            <div
+                                className={css({
+                                    background: 'white',
+                                    borderRadius: '4px',
+                                    boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+                                    transition: 'all 0.3s cubic-bezier(.25,.8,.25,1)',
+                                    textAlign: 'center',
+                                })}
+                            >
+                                {/*
+                                {console.log(types)}
+                                {console.log(types)} */}
+                                {/* <div>
+                                    {sprites.map((index: any) => {
+                                        console.log(index)
+                                        return(
+                                            <img src="img_girl.jpg" alt="Girl in a jacket" width="500" height="600" />
+                                        )
+                                    })}
+                                </div> */}
+                                <div>
+                                    {game_indices.map((index: any) => {
+                                        return (
+                                            <Button key={index?.version?.name} shape={SHAPE.pill}>{index?.version?.name}</Button>
+                                        )
+                                    })}
+                                </div>
+                            </div>
                         </Cell>
                         <Cell span={[6, 3, 4]}>
                             <div
@@ -117,20 +146,47 @@ const PokemonId: React.FC<PokemonProps> = ({ match }: PokemonProps) => {
                                     borderRadius: '4px',
                                     boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
                                     transition: 'all 0.3s cubic-bezier(.25,.8,.25,1)',
+                                    textAlign: 'center',
                                 })}
                             >
-                                <ListItem
-                                    endEnhancer={() => (
-                                        <ListItemLabel><Alert /></ListItemLabel>
-                                    )}
-                                >
-                                    {abilities.map((index: any) =>{
-                                        return(
-                                            console.log(index)
-                                        );
+                                <div>
+                                    {types.map((index: any) => {
+                                        return (
+                                            <Link
+                                                key={index?.slot}
+                                                to={`${'/'}`}
+                                                className={css({ textDecoration: 'none', color: '#000' })}
+                                            >
+                                                <Tag
+                                                    closeable={false}
+                                                    variant={VARIANT.solid}
+                                                    size={SIZE.medium}
+                                                >
+                                                    {index?.type?.name}
+                                                </Tag>
+                                            </Link>
+                                        )
                                     })}
-                                </ListItem>
-
+                                </div>
+                                <div>
+                                    {abilities.map((index: any) => {
+                                        return (
+                                            <Link
+                                                key={index?.slot}
+                                                to={`${'/'}`}
+                                                className={css({ textDecoration: 'none', color: '#000' })}
+                                            >
+                                                <Tag
+                                                    closeable={false}
+                                                    variant={VARIANT.solid}
+                                                    size={SIZE.medium}
+                                                >
+                                                    {index?.ability?.name}
+                                                </Tag>
+                                            </Link>
+                                        )
+                                    })}
+                                </div>
                             </div>
                         </Cell>
                     </Grid>
